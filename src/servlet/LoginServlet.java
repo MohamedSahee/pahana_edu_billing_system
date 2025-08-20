bef25da296f0f70b0660f0ec26404516b9566c84
mohamedsahee <msahee35@gmail.com>
2025-08-20 15:17:36 +0530

    login servlet code update

---
+++ src/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.User;
+import dao.UserDAO;
+import dao.UserDAOImpl;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UserDAO userDAO = new UserDAOImpl();
+        User user = userDAO.getUserByUsername(username);
+
+        if (user != null && user.getPassword().equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            session.setAttribute("userRole", user.getRole());
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            response.sendRedirect("index.jsp?error=1");
+        }
+    }
+}
